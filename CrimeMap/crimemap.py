d09a0d93cb9e891f81b647f80c16ff68daf14319
Hidesato Ikeya <iky.hdst@gmail.com>
2017-12-26 21:25:11 +0900

    crimemap: added input validation

--- CrimeMap/crimemap.py
+++ CrimeMap/crimemap.py
@@ -1,30 +1,33 @@
+import dateparser
 import flask
 import dbconfig
 import json
 from dbcrime import DBCrime
 
-
 app = flask.Flask(__name__)
 DB = DBCrime()
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
     return flask.render_template(
-        "home.html", map_key=dbconfig.gmap_key, crimes=crimes
+        "home.html",
+        map_key=dbconfig.gmap_key,
+        crimes=crimes,
+        categories=DBCrime.categories,
+        error_message=error_message,
     )
-    # p.141
 
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = flask.request.form.get("category")
     date = flask.request.form.get("date")
-    latitude = float(flask.request.form.get("latitude"))
-    longitude = float(flask.request.form.get("longitude"))
+    latitude = flask.request.form.get("latitude")
+    longitude = flask.request.form.get("longitude")
     description = flask.request.form.get("description")
-    DB.add_crime(category, date, latitude, longitude, description)
-    return home()
+    err = DB.add_crime(category, date, latitude, longitude, description)
+    return home(err)
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
--- CrimeMap/dbcrime.py
+++ CrimeMap/dbcrime.py
@@ -1,8 +1,11 @@
 import datetime
+import dateparser
 import dbconfig
+import string
 from dbhelper import DBHelper
 
 class DBCrime(DBHelper):
+    categories = ['magging', 'break-in']
     def __init__(self):
         super().__init__(dbconfig.db_user,
                          dbconfig.db_password,
@@ -11,16 +14,39 @@ class DBCrime(DBHelper):
 
     def add_crime(self, category, date,
                   latitude, longitude, description):
+        try:
+            if category not in self.categories:
+                raise RuntimeError(f"invalid category: {category}")
+            date = self.format_date(date)
+            latitude = float(latitude)
+            longitude = float(longitude)
+        except Exception as err:
+            print(self.add_crime.__qualname__, ":", err)
+            return err
+
+        description = self.sanitize_string(description)
+
         with self as connection:
             query = \
               "INSERT INTO crimes (category, date, latitude, longitude, description)" \
               "VALUES (%s, %s, %s, %s, %s)"
             with connection.cursor() as cursor:
                 cursor.execute(
-                  query,
-                  (category, date, latitude, longitude, description)
+                    query,
+                    (category, date, latitude, longitude, description)
                 )
                 connection.commit()
+                return
+
+    @staticmethod
+    def format_date(userdate):
+        date = dateparser.parse(userdate)
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+
+    @staticmethod
+    def sanitize_string(userinput):
+        whitelist = string.ascii_letters + string.digits + " !?$.,:;-'()&"
+        return "".join(filter(lambda x: x in whitelist, userinput))
 
     def get_all_crimes(self):
         cols = ('latitude', 'longitude', 'date', 'category', 'description')
