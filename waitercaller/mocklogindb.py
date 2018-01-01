4c0589b2313366abbdd26a970f1ac1cef63f24b5
Hidesato Ikeya <iky.hdst@gmail.com>
2018-01-01 21:34:19 +0900

    waitercaller use WTForm

--- waitercaller/mocklogindb.py
+++ waitercaller/mocklogindb.py
@@ -1,9 +1,27 @@
+import datetime
+import config
 import dbconfig
 import passwordhelper
 from user import User
 from dbhelper import DBHelper
 
 MOCK_USERS = {'test@example.com': '123456'}
+MOCK_TABLES = [
+    {
+        "_id": passwordhelper.get_hash("1"),
+        "name": "1",
+        "owner": "test@example.com",
+        "url": "mockurl"
+    }
+]
+MOCK_REQUESTS = [
+    {
+        "_id": "1",
+        "table_id": "1",
+        "table_name": "1",
+        "time": datetime.datetime.now()
+    }
+]
 
 class MockLoginDB(DBHelper):
     def __init__(self):
@@ -18,10 +36,8 @@ class MockLoginDB(DBHelper):
             with connection.cursor() as cursor:
                 cursor.execute(query, email)
                 for user in cursor:
-                    if user:
-                        return User(email, user[0], user[1])
-                    else:
-                        return None
+                    return User(email, user[0], user[1])
+                return None
 
     def add_user(self, email, password):
         salt = passwordhelper.get_salt()
@@ -32,5 +48,60 @@ class MockLoginDB(DBHelper):
             with connection.cursor() as cursor:
                 cursor.execute(query, (email, salt, hash))
                 connection.commit()
+                return True
+        return False
+
+    def add_table(self, table_name, owner_id):
+        _id = passwordhelper.get_hash(owner_id + table_name)[:5]
+        MOCK_TABLES.append({
+            "_id": _id,
+            "name": table_name,
+            "owner": owner_id,
+            "url": config.base_url + "newrequest/" + _id
+        })
+        return _id
+
+    def get_tables(self, owner_id):
+        return MOCK_TABLES
+
+    #def update_table(self, table_id, url):
+    #    for table in MOCK_TABLES:
+    #        if table.get("_id") == table_id:
+    #            table["url"] = url
+    #            break
+
+    def delete_table(self, table_id):
+        for i, table in enumerate(MOCK_TABLES):
+            if table["_id"] == table_id:
+                del MOCK_TABLES[i]
+                return True
+        return False
+
+    def add_request(self, table_id, time):
+        for table in MOCK_TABLES:
+            if table["_id"] == table_id:
+                break
+        else:
+            return False
+        for i, req in enumerate(MOCK_REQUESTS):
+            if req["table_id"] == table_id:
+                del MOCK_REQUESTS[i]
+                break
+        _id = passwordhelper.get_hash(table_id)[:5]
+        MOCK_REQUESTS.append({
+            "_id": _id,
+            "table_id": table["_id"],
+            "table_name": table["name"],
+            "time": time
+        })
+        return True
+
+    def get_requests(self, owner_id):
+        return MOCK_REQUESTS
+
+    def delete_request(self, request_id):
+        for i, request in enumerate(MOCK_REQUESTS):
+            if request["_id"] == request_id:
+                del MOCK_REQUESTS[i]
                 return True
         return False
--- waitercaller/waitercaller.py
+++ waitercaller/waitercaller.py
@@ -1,8 +1,10 @@
+import datetime
 import flask
 import flask_login
 from mocklogindb import MockLoginDB as LoginDB
 from user import User
 from passwordhelper import validate_password
+from forms import RegistrationForm, LoginForm, CreateTableForm
 
 DB = LoginDB()
 
@@ -12,38 +14,98 @@ login_manager = flask_login.LoginManager(app)
 
 @app.route("/")
 def home():
-    return flask.render_template("home.html")
+    return flask.render_template(
+        "home.html", loginform=LoginForm(),
+        registrationform=RegistrationForm()
+    )
+
 
 @app.route("/login", methods=["POST"])
 def login():
-    email = flask.request.form.get('email')
-    password = flask.request.form.get('password')
-    user = DB.get_user(email)
-    if user and validate_password(password, user.salt, user.hash):
-        flask_login.login_user(user, remember=True)
-        return flask.redirect(flask.url_for('account'))
-    else:
-        return home()
+    form = LoginForm(flask.request.form)
+    if form.validate():
+        user = DB.get_user(form.email.data)
+        if user and validate_password(form.password.data, user.salt, user.hash):
+            flask_login.login_user(user, remember=True)
+            return flask.redirect(flask.url_for('account'))
+        else:
+            form.email.errors.append("Email or password invalid")
+    return flask.render_template(
+        "home.html", loginform=form,
+        registrationform=RegistrationForm()
+    )
 
 @app.route("/register", methods=['POST'])
 def register():
-    email = flask.request.form.get('email')
-    password = flask.request.form.get('password')
-    password2 = flask.request.form.get('password2')
-    if password != password2:
-        return "Passwords unmatch."
-    DB.add_user(email, password)
-    return home()
+    form = RegistrationForm(flask.request.form)
+    if form.validate():
+        if DB.get_user(form.email.data):
+            form.email.errors.append("Email address already registered")
+            return flask.render_template('home.html', registrationform=form)
+        DB.add_user(form.email.data, form.password.data)
+        return flask.render_template(
+            "home.html", loginform=LoginForm(),
+            registrationform=form,
+            onloadmessage="Registration successful. Please log in.")
+    return flask.render_template(
+        "home.html", loginform=LoginForm(),
+        registrationform=form
+    )
 
 @app.route("/logout")
 def logout():
     flask_login.logout_user()
     return flask.redirect(flask.url_for('home'))
 
+@app.route("/dashboard")
+@flask_login.login_required
+def dashboard():
+    now = datetime.datetime.now()
+    requests = DB.get_requests(flask_login.current_user.get_id())
+    for req in requests:
+        deltaseconds = (now - req['time']).seconds
+        req['wait_minutes'] = f"{deltaseconds // 60}:{deltaseconds % 60:02}"
+    return flask.render_template("dashboard.html", requests=requests)
+
 @app.route("/account")
 @flask_login.login_required
 def account():
-    return "You are logged in"
+    return flask.render_template(
+        "account.html", createtableform=CreateTableForm(),
+        tables=DB.get_tables(flask_login.current_user.get_id())
+    )
+
+@app.route("/account/createtable", methods=["POST"])
+@flask_login.login_required
+def account_createtable():
+    form = CreateTableForm(flask.request.form)
+    if form.validate():
+        DB.add_table(form.tablename.data, flask_login.current_user.get_id())
+        return flask.redirect(flask.url_for('account'))
+    return flask.render_template(
+        "account.html", createtableform=form,
+        tables=DB.get_tables(flask_login.current_user.get_id())
+    )
+
+@app.route("/account/deletetable")
+@flask_login.login_required
+def account_deletetable():
+    table_id = flask.request.args.get("tableid")
+    DB.delete_table(table_id)
+    return flask.redirect(flask.url_for('account'))
+
+@app.route("/dashboard/resolve")
+@flask_login.login_required
+def dashboard_resolve():
+    request_id = flask.request.args.get("request_id")
+    DB.delete_request(request_id)
+    return flask.redirect(flask.url_for('dashboard'))
+
+@app.route("/newrequest/<table_id>")
+def new_request(table_id):
+    DB.add_request(table_id, datetime.datetime.now())
+    return "Your request has been logged and a waiter will be with you shortly."
+
 
 @login_manager.user_loader
 def load_user(user_id):
